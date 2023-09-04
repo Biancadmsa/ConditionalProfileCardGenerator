bbe72ecf44badcfb847547d6aeaaa2dfb9045929
Biancadmsa Salcedo <biancadmsadf16@gmail.com>
2023-09-04 21:25:00 +0000

    miprimercommit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
-import "../style/index.css";
+// import "../style/index.css";
+
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
@@ -32,15 +33,15 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <img src="${variables.avatarURL}" class="photo" />
+            <h1>${variables.name} ${variables.lastname}</h1>
+            <h2>${variables.role}</h2>
+            <h3>${variables.city} ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">    
+            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -59,6 +60,7 @@ window.onload = function() {
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
+    
     // social media usernames
     twitter: null,
     github: "alesanchezr",
